8b9415faaabf7c2da33245c05c71410f792766e7
majda <majda.nasupovic@carwow.co.uk>
2026-03-18 11:57:00 +0000

    Add Authentication
    - Generate the Authentication by running command
    ```
    bin/rails generate authentication
    ```
    
    as per documentation https://guides.rubyonrails.org/getting_started.html#adding-authentication
    
    - Run rubocop autofix on updated and newly created files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+#
+# Base controller for the application.
 class ApplicationController < ActionController::Base
+  include Authentication
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
