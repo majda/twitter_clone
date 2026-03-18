e67edf4dfe691ecc34d0b82bfae03308f6929d94
majda <majda.nasupovic@carwow.co.uk>
2026-03-18 12:51:44 +0000

    Fix broken test

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Tweets', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/tweets/index'
+      get tweets_path
       expect(response).to have_http_status(:success)
     end
   end
