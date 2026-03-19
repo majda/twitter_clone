7c597ec82e15f4673c7645d0a8a0d67bfe25605c
majda <majda.nasupovic@carwow.co.uk>
2026-03-19 12:00:17 +0000

    Add user to the Tweet table, which is a foreign key of the User.id from User table
    This is to connect users with tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 20_260_319_105_735) do
+ActiveRecord::Schema[8.1].define(version: 20_260_319_115_524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_catalog.plpgsql'
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[8.1].define(version: 20_260_319_105_735) do
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.bigint 'user_id', null: false
+    t.index ['user_id'], name: 'index_tweets_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[8.1].define(version: 20_260_319_105_735) do
   end
 
   add_foreign_key 'sessions', 'users'
+  add_foreign_key 'tweets', 'users'
 end
