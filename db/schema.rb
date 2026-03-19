47c4fd2ae6557fd1bb7b5e2e2fffa7a7b8598f2b
majda <majda.nasupovic@carwow.co.uk>
2026-03-19 11:11:13 +0000

    Add name column to the existing users table

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 20_260_318_104_046) do
+ActiveRecord::Schema[8.1].define(version: 20_260_319_105_735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_catalog.plpgsql'
 
@@ -34,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 20_260_318_104_046) do
   create_table 'users', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.string 'email_address', null: false
+    t.string 'name'
     t.string 'password_digest', null: false
     t.datetime 'updated_at', null: false
     t.index ['email_address'], name: 'index_users_on_email_address', unique: true
