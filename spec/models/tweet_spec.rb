ebaf73ab911047f0977cf84fa6eba1a389358a2d
majda <majda.nasupovic@carwow.co.uk>
2026-03-20 11:43:06 +0000

    Add basic tests for the tweet model

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,5 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create!(
+      email_address: 'author@example.com',
+      password: 'password123',
+      name: 'Author'
+    )
+  end
+
+  subject { Tweet.new(body: 'This my first tweet', user: user) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a body' do
+    subject.body = nil
+    expect(subject).to_not be_valid
+  end
 end
