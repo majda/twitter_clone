925936d90d3dafd9330553ee2cb502e16b5cae9a
majda <majda.nasupovic@carwow.co.uk>
2026-03-20 11:05:03 +0000

    Add tests for the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(email_address: 'test@test.com', password: 'test123', name: 'John') }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a email_address' do
+    subject.email_address = nil
+    expect(subject).to_not be_valid
+  end
+
+  it('is not valid without a name') do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'rejects a duplicate email_address' do
+    subject.save!
+    dup_user = subject.dup
+    expect(dup_user).not_to be_valid
+    expect(dup_user.errors[:email_address]).to include('is already in use')
+  end
 end
