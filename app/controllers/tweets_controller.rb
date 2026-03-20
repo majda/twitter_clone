ecb72d2bce379f367dadcd7d28b6cf3f1ccf6e4c
majda <majda.nasupovic@carwow.co.uk>
2026-03-20 08:50:29 +0000

    Set user_id when creating new tweets as well as list only tweets which have user assigned

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+#
+# Handles listing, creating, updating, and deleting tweets.
 class TweetsController < ApplicationController
   allow_unauthenticated_access only: %i[index show]
   before_action :set_tweet, only: %i[show edit update destroy]
   def index
-    @tweets = Tweet.all
+    # @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order(created_at: :desc)
   end
 
   def show; end
@@ -14,7 +17,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = Current.user.tweets.build(tweet_params)
     if @tweet.save
       redirect_to @tweet
     else
