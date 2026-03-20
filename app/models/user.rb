190291eb184ec769fcd467a2c11948e5c40489b8
majda <majda.nasupovic@carwow.co.uk>
2026-03-20 08:49:18 +0000

    Set association in the User and Tweet models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
+#
+# Application user account with authentication and authored tweets.
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :tweets
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
